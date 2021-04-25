eea16641d5d6fd36eb2011f453c621fbad19bdc1
阿司匹林 <1411373683@qq.com>
2021-04-25 13:51:52 +0800

    完成了登录功能

---
+++ src/plugins/element.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import { Message, Input, Button, Form, FormItem } from 'element-ui'
+// 导入弹窗提示
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+// 导入全局样式表
+import '../assets/css/global.css'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/Login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
-
+router.beforeEach((to, form, next) => {
+  // to  将要访问的函数
+  // form 代表从哪个路径跳转而来
+  // next 是个函数 表示放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
